034cf934fb8540fb766e9e6d981611153d743f4e
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-04-26 16:12:06 +0200

    Added DB connection

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import http from "http";
 import { Server } from "socket.io";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+const url_db = process.env.DB_HOST;
 
 app.set("view engine", "ejs").set("views", path.join(__dirname, "./views"));
 app.use(express.static(__dirname + "/public")).use(express.urlencoded());
@@ -27,6 +31,15 @@ app.get("/room", (req, res) => {
 
 app.get("/room/:roomId", (req, res) => {});
 
-app.get("/admin");
+app.get("/admin", (req, res) => {
+	res.render("admin");
+});
 
 app.post("/request");
+
+connectDB().catch((err) => console.log(err));
+
+async function connectDB() {
+	const connection = await mongoose.connect(url_db);
+	console.log(typeof mongooses);
+}
