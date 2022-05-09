b383d1352bb9d780c1428a0f96f61973d2d45119
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 09:38:33 +0200

    Clubrouter update for users dashboard

--- Routers/clubRouter.js
+++ Routers/clubRouter.js
@@ -1,6 +1,11 @@
 import express from "express";
-import Club from "../models/Club.js";
-import { club_create } from "../controllers/clubsController.js";
+import { checkAuthenticated } from "../controllers/passport.config.js";
 
 const app = express();
 export const router = express.Router();
+
+router.get("/:club/admin", checkAuthenticated, (req, res) => {
+	res.render("admin", {
+		user: req.user,
+	});
+});
