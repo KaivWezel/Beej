1a092bb026474aff8459e50f9cabff4c7a3ee3cf
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 09:38:46 +0200

    Applied passport and authentication

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import * as path from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
 import mongoose from "mongoose";
+import passport from "passport";
 import { router as authRouter } from "./Routers/authRouter.js";
+import { router as clubRouter } from "./Routers/clubRouter.js";
+import { initializePassport } from "./controllers/passport.config.js";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import passport from "passport";
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -22,43 +24,46 @@ server.listen(PORT, () => {
 	console.log(`listening to port: ${PORT}`);
 });
 
+// Databse connection
+const connection = await mongoose.connect(url_db);
+if (connection) {
+	console.log("mongodb ready");
+}
+
 app.set("view engine", "ejs").set("views", path.join(__dirname, "./views"));
+
 app
 	.use(express.static(path.join(__dirname, "/public")))
 	.use(express.urlencoded())
-	.use(express.json())
-	.use(
-		session({
-			secret: process.env.SESSION_SECRET,
-			resave: false,
-			saveUninitialized: false,
-			store: MongoStore.create({
-				mongoUrl: url_db,
-				touchAfter: 48 * 3600,
-			}),
-		})
-	);
+	.use(express.json());
 
 // passport middlewares
-app.use(passport.initialize()).use(passport.session());
+initializePassport(passport);
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({
+			mongoUrl: url_db,
+			ttl: 60 * 60,
+		}),
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Router middlewares
-app.use(authRouter);
+app.use(authRouter).use(clubRouter);
 
 // Routes
-app.get("/", (req, res) => {
-	res.render("index");
-});
-
-// Databse connection
-export const db = async () => {
-	try {
-		const connection = await mongoose.connect(url_db);
-		console.log("MongoDb connected");
-		return connection;
-	} catch (err) {
-		console.log(err);
+app.get(
+	"/",
+	(req, res, next) => {
+		console.log(req.user);
+		next();
+	},
+	(req, res) => {
+		res.render("index");
 	}
-};
-
-db();
+);
