8bc24676caa688b90a338a92265f23a730228888
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 11:01:08 +0200

    UPdate routers

--- Routers/clubRouter.js
+++ Routers/clubRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { checkAuthenticated } from "../controllers/passport.config.js";
 
-const app = express();
 export const router = express.Router();
 
 router.get("/:club/admin", checkAuthenticated, (req, res) => {
