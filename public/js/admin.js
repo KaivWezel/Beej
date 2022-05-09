888784d3a7b3e3343e6bd71664fac1dbf2c4bc67
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 12:22:46 +0200

    FIxed fetch requests for server toggle

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,23 +5,24 @@ const openRoom = document.querySelector(".open-room");
 const closeRoom = document.querySelector(".close-room");
 const url = "localhost:3000";
 
+const data = { opened: true };
+const string = JSON.stringify(data);
 // Eventlisteners
 openRoom.addEventListener("click", async (e) => {
 	e.preventDefault();
 	const response = await fetch("http://localhost:3000/room", {
 		headers: {
-			accept:
-				"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+			accept: "application/json",
 			"accept-language": "en-US,en;q=0.9,nl;q=0.8",
 			"cache-control": "max-age=0",
-			"content-type": "application/x-www-form-urlencoded",
+			"content-type": "application/json",
 			"sec-fetch-site": "same-origin",
 			"sec-fetch-user": "?1",
 			"upgrade-insecure-requests": "1",
 		},
 		referrer: "http://localhost:3000/shots/admin",
 		referrerPolicy: "strict-origin-when-cross-origin",
-		body: null,
+		body: JSON.stringify({ opened: true }),
 		method: "POST",
 		mode: "cors",
 		credentials: "include",
@@ -31,19 +32,20 @@ openRoom.addEventListener("click", async (e) => {
 
 closeRoom.addEventListener("click", async (e) => {
 	e.preventDefault();
-	const response = await fetch("http://localhost:3000/room?", {
+	const response = await fetch("http://localhost:3000/room", {
 		headers: {
-			accept:
-				"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+			accept: "application/json",
 			"accept-language": "en-US,en;q=0.9,nl;q=0.8",
+			"cache-control": "max-age=0",
+			"content-type": "application/json",
 			"sec-fetch-site": "same-origin",
 			"sec-fetch-user": "?1",
 			"upgrade-insecure-requests": "1",
 		},
 		referrer: "http://localhost:3000/shots/admin",
 		referrerPolicy: "strict-origin-when-cross-origin",
-		body: null,
-		method: "GET",
+		body: JSON.stringify({ opened: false }),
+		method: "POST",
 		mode: "cors",
 		credentials: "include",
 	});
