de620cf71e2780c3e040c9245705290f5b2e1366
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 17:07:55 +0200

    Added router for Rooms

--- Routers/roomRouter.js
+++ Routers/roomRouter.js
@@ -1,13 +1,34 @@
 import express from "express";
-import { openRoom } from "../controllers/roomController.js";
-import Bid from "../models/Bid.js";
 import User from "../models/User.js";
+import Slot from "../models/Slot.js";
+
+import {
+	checkRoom,
+	openRoom,
+	createSlot,
+} from "../controllers/roomController.js";
+
 export const router = express.Router();
 
-router.post("/room", express.json(), async (req, res) => {
-	console.log(req.body);
-	const user = await User.findOne({ id: req.user.id });
-	user.room.opened = req.body.opened;
-	await user.save();
-	res.send("<h1>hellow</h1>");
+router.post("/room", openRoom, (req, res) => {
+	// res.redirect(`/${req.user.username}/admin`);
+	res.redirect("/");
+});
+
+router.post("/join", (req, res) => {
+	res.redirect(`/${req.body.room}`);
+});
+
+router.post("/slot", createSlot, (req, res) => {
+	res.redirect(`/${req.user.username}/admin`);
+});
+
+router.get("/:room", checkRoom, async (req, res) => {
+	const slots = await Slot.find({ _id: req.user.room.slots });
+	console.log(slots);
+
+	res.render("room", {
+		roomId: req.params.room,
+		slots: slots,
+	});
 });
