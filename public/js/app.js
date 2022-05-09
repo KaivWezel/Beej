7db74fb1535e675a32ec9b666f2d96316ee2df11
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-09 17:08:14 +0200

    Started sockets

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 console.log("appje werkt");
+
+const socket = io();
+
+window.onload = (e) => {
+	const path = window.location.pathname;
+	const roomId = path.split("").splice(1).join("");
+	socket.emit("join-room", roomId);
+};
